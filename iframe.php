8a52dc92e7ba1d4ebc997cb005f933f91d42ad90
vicky1242 <143662205+vicky1242@users.noreply.github.com>
2023-09-09 00:37:11 +0500

    iframe code
    iframe code

---
+++ iframe.php
@@ -0,0 +1,12 @@
+
+<?php
+$curl_handle=curl_init();
+curl_setopt($curl_handle, CURLOPT_URL,$_REQUEST['url']);
+curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+$query = curl_exec($curl_handle);
+curl_close($curl_handle);
+print_r($query);
+exit();
+?>
