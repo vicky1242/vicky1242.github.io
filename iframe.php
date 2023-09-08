aed92845a3d311b9cad12301bb9e61370bad053f
vicky1242 <143662205+vicky1242@users.noreply.github.com>
2023-09-09 00:46:05 +0500

    Update iframe.php

--- iframe.php
+++ iframe.php
@@ -8,5 +8,23 @@ curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
 $query = curl_exec($curl_handle);
 curl_close($curl_handle);
 print_r($query);
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Iframe site</title>
+</head>
+<body>
+	<style type="text/css">
+		iframe {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			border: 2px solid #000;			
+		}
+	</style> 
+	<iframe src="<?php $query?>.https://gablesfurniture.com/"></iframe>
+</body>
+</html>
